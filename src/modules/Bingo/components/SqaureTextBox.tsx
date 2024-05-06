34632a09a9e7eb13c36854cf13c1be98cab79e77
ehddnr301 <dy950328@gmail.com>
2024-05-06 08:09:13 +0000

    feat(bingo): Edit Bingo SquareTextBox
    체크된 빙고는 동그라미 표시를 해주었습니다.
    
    (#6 - 3)

--- src/modules/Bingo/components/SqaureTextBox.tsx
+++ src/modules/Bingo/components/SqaureTextBox.tsx
@@ -1,24 +1,62 @@
 import { Container, Typography, Theme } from "@mui/material";
-import { styled } from "@mui/system";
+import { styled, keyframes } from "@mui/system";
 
 const Wrapper = styled("div")<SquareTextProps>(({ status }) => ({
     border: "1px solid black",
     borderRadius: "3px",
     margin: "2px",
-    backgroundColor: "#E2E7F0",
     fontSize: "12px",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    color: `${status === 1 ? "red" : "black"}`,
+    color: `${status === 1 ? "#FD7329" : "#3E64FF"}`,
+    backgroundColor: "#f8f9fa", 
+    position: "relative", 
+    boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.16)",
     height: "60px",
     width: "60px",
-    '@media (min-width: 768px)': {  // PC 화면 사이즈를 768px 이상으로 가정
-        width: "200px",  // PC에서 사용할 너비
-        height: "200px",  // PC에서 사용할 높이
+    '@media (min-width: 768px)': { 
+        width: "200px", 
+        height: "200px", 
         fontSize: "24px",
       }
 }));
+
+const BingoCircle = styled("div")({
+    position: "absolute",
+    width: "100%",
+    height: "98%",
+    borderRadius: "50%",
+    animation: `${keyframes({
+        "0%": {
+            transform: "rotate(0deg); translate(0, 0)"
+          },
+          "33%":  {
+            transform: "rotate(360deg); translate(0.375rem, 0.375rem)"
+          },
+          "66%":  {
+            transform: "rotate(720deg); translate(-0.375rem, -0.375rem)"
+          },
+          "100%":  {
+            transform: "rotate(1080deg); translate(0, 0);"
+          }
+    })} 4s infinite`,
+  });
+
+// Specific styles for each circle type
+const FirstCircle = styled(BingoCircle)({
+    boxShadow: "0 -1px 0 0 rgba(255, 64, 0, 0.25), 1px 0 0 0 rgba(255, 192, 0, 0.25), 0 1px 0 0 rgba(255, 192, 0, 0.25), -1px 0 0 0 rgba(255, 64, 0, 0.25), 1px -1px 0 0 rgba(255, 128, 0, 0.5), -1px 1px 0 0 rgba(255, 128, 0, 0.5), -1px -1px 0 0 rgba(255, 0, 0, 0.75), 1px 1px 0 0 rgba(255, 255, 0, 0.75)",
+});
+
+const SecondCircle = styled(BingoCircle)({
+    animationDelay: "0.5s",
+    boxShadow: "0 -1px 0 0 rgba(165, 181, 222, 0.25), 1px 0 0 0 rgba(225, 131, 194, 0.25), 0 1px 0 0 rgba(225, 131, 194, 0.25), -1px 0 0 0 rgba(165, 181, 222, 0.25), 1px -1px 0 0 rgba(195, 156, 208, 0.5), -1px 1px 0 0 rgba(195, 156, 208, 0.5), -1px -1px 0 0 rgba(135, 206, 235, 0.75), 1px 1px 0 0 rgba(255, 105, 180, 0.75)",
+});
+
+const ThirdCircle = styled(BingoCircle)({
+    animationDelay: "0.8s",
+    boxShadow: "0 -1px 0 0 rgba(0, 255, 255, 0.25), 1px 0 0 0 rgba(0, 255, 255, 0.25), 0 1px 0 0 rgba(0, 255, 255, 0.25), -1px 0 0 0 rgba(0, 255, 255, 0.25), 1px -1px 0 0 rgba(0, 255, 255, 0.5), -1px 1px 0 0 rgba(0, 255, 255, 0.5), -1px -1px 0 0 rgba(0, 255, 255, 0.75), 1px 1px 0 0 rgba(0, 255, 255, 0.75)",
+});
 
 
 type SquareTextProps = {
@@ -28,7 +66,16 @@ type SquareTextProps = {
 
 const SquareTextBox = (props: SquareTextProps) => {
     return (
-        <Wrapper {...props}>{props.value}</Wrapper>
+        <Wrapper {...props}>
+        {props.status === 1 && (
+          <BingoCircle>
+                      <FirstCircle />
+          <SecondCircle />
+          <ThirdCircle />
+          </BingoCircle>
+        )}
+        {props.value}
+      </Wrapper>
         );
 };
 
