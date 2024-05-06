bacb1e6fdb499b7c71369e05e94464f6d46ca063
4roring <4roring@naver.com>
2024-05-06 17:04:54 +0900

    body 컨텐츠 전체에 padding-top 추가, 회원가입 폼 완료

--- src/modules/Header/SignUpForm.tsx
+++ src/modules/Header/SignUpForm.tsx
@@ -1,15 +1,57 @@
-import { Box, Avatar, IconButton, Modal, Typography } from "@mui/material";
+import { Box, TextField, FormControlLabel, Checkbox, Link, Button } from "@mui/material";
+
 
 interface SignUpProps {
-
+    email: string // 소셜 로그인으로 자동으로 추가될 수 있다
 }
 
 
 const SignUpForm = (signUpProps: SignUpProps) => {
 
     return (
-        <Box>
-            <div>asdf</div>
+        <Box component="form" sx={{
+            '& .MuiTextField-root': { m: 1, width: '50vw', textAlign: "center" },
+        }}>
+            <TextField
+                required
+                id="outlined-required"
+                label="이름"
+                placeholder="본명으로 입력"
+            />
+            <br />
+
+            <TextField
+                required
+                id="outlined-required"
+                label="닉네임"
+                placeholder="2글자 이상 알파벳, 한글, 숫자"
+            />
+            <br />
+
+            <TextField
+                id="outlined"
+                variant="outlined"
+                label="전화번호"
+                placeholder="010XXXXXXXX"
+            />
+            <br />
+
+            <TextField
+                id="outlined"
+                label="이메일"
+                defaultValue={signUpProps.email}
+                placeholder="example@pseudolab.com"
+            />
+            <br />
+
+            <Box sx={{ marginTop: "3vw" }}>
+                <FormControlLabel required control={<Checkbox />} label="개인 정보 수집 및 이용 동의" />
+                <Link href="/">보기</Link>
+            </Box>
+            <br />
+
+            <Button variant="outlined">가입 하기</Button>
+
         </Box>
     );
 };
