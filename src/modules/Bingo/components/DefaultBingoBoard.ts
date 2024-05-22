8ead2ae77e7bbd31d3bc739957e7ac69cccad7cf
ehddnr301 <dy950328@gmail.com>
2024-05-22 10:39:09 +0000

    feat(bingo): Change bingo list
    빙고 리스트 진수님께서 제안주신 리스트로 변경하였습니다.

--- src/modules/Bingo/components/DefaultBingoBoard.ts
+++ src/modules/Bingo/components/DefaultBingoBoard.ts
@@ -13,27 +13,57 @@ export const defafultBingoBoard = [
   {
     selected: 0,
     status: 0,
-    value: "AI 스타트업 재직자",
+    value: "기술 블로그 운영자",
   },
   {
     selected: 0,
     status: 0,
-    value: "AI 아티스트",
+    value: "Linkedin 사용자",
   },
   {
     selected: 0,
     status: 0,
-    value: "AI 뉴스레터 구독자",
+    value: "데이터 분석가/과학자",
   },
   {
     selected: 0,
     status: 0,
-    value: "AI 유튜브 채널 구독자",
+    value: "PM/PO",
   },
   {
     selected: 0,
     status: 0,
-    value: "프론트엔드, 백엔드 엔지니어",
+    value: "8기 스터디 참여자",
+  },
+  {
+    selected: 0,
+    status: 0,
+    value: "인과추론 번역서 독자",
+  },
+  {
+    selected: 0,
+    status: 0,
+    value: "A/B 테스트 경험자",
+  },
+  {
+    selected: 0,
+    status: 0,
+    value: "제품 개선 경험자",
+  },
+  {
+    selected: 0,
+    status: 0,
+    value: "인과추론팀 깃헙 팔로워",
+  },
+  {
+    selected: 0,
+    status: 0,
+    value: "대시보드 만들어본 사람",
+  },
+  {
+    selected: 0,
+    status: 0,
+    value: "통계/경제 전공자",
   },
   {
     selected: 0,
@@ -43,96 +73,66 @@ export const defafultBingoBoard = [
   {
     selected: 0,
     status: 0,
-    value: "SNS 사용자",
+    value: "SQL 사용자",
   },
   {
     selected: 0,
     status: 0,
-    value: "직전 경력 특이한",
+    value: "데이터 컨퍼런스 참가경험",
   },
   {
     selected: 0,
     status: 0,
-    value: "AI 모델 배포 경험자",
+    value: "인과추론 연구자",
   },
   {
     selected: 0,
     status: 0,
-    value: "기술 블로그 운영자",
+    value: "CHATGPT 사용자",
   },
   {
     selected: 0,
     status: 0,
-    value: "해외 컨퍼런스 참가경험",
+    value: "OP.GG 써본 사람",
   },
   {
     selected: 0,
     status: 0,
-    value: "귀여운 IT 굿즈 받아본자",
+    value: "배그 해본 사람",
   },
   {
     selected: 0,
     status: 0,
-    value: "Welcome to PseudoCon",
+    value: "게임이 취미인 사람",
   },
   {
     selected: 0,
     status: 0,
-    value: "부트캠프 참가경험",
+    value: "MBTI E인 사람",
   },
   {
     selected: 0,
     status: 0,
-    value: "AI 관련 해커톤 경험자",
+    value: "반려동물 키우는 사람",
   },
   {
     selected: 0,
     status: 0,
-    value: "올해를 알차게 보낸자",
+    value: "아이폰 쓰는 사람",
   },
   {
     selected: 0,
     status: 0,
-    value: "AI 윤리/정책 연구자",
+    value: "넷플릭스 구독자",
   },
   {
     selected: 0,
     status: 0,
-    value: "로봇/드론 관련 연구자",
+    value: "오늘 저녁 약속있는 사람",
   },
   {
     selected: 0,
     status: 0,
-    value: "내 MBTI 설명 가능한자",
-  },
-  {
-    selected: 0,
-    status: 0,
-    value: "VR/AR 헤드셋 보유자",
-  },
-  {
-    selected: 0,
-    status: 0,
-    value: "Kaggle 또는 Dacon 우승자",
-  },
-  {
-    selected: 0,
-    status: 0,
-    value: "멀티모달 관련 연구자",
-  },
-  {
-    selected: 0,
-    status: 0,
-    value: "특별한 경험을 가진자",
-  },
-  {
-    selected: 0,
-    status: 0,
-    value: "DevOps, MLOps, SRE",
-  },
-  {
-    selected: 0,
-    status: 0,
-    value: "이전 기수 참가자",
+    value: "KPOP 좋아하는 사람",
   },
 ];
