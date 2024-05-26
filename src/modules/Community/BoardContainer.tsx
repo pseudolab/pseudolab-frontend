aee7d8a200b4922c060e0283f278e976c1ddcc82
4roring <4roring@naver.com>
2024-05-26 18:39:17 +0900

    게시판 작업중이던 부분까지 커밋

--- src/modules/Community/BoardContainer.tsx
+++ src/modules/Community/BoardContainer.tsx
@@ -7,7 +7,7 @@ const DUMMY_BOARD_LIST: BoardItemProps[] = [
   {
     title: "테스트1",
     author: "kwang1",
-    createdAt: Date.now(),
+    createdAt: new Date("2024-03-10T13:19:11+0000").getTime(),
     viewCount: 5,
     commentCount: 2,
     likeCount: 3,
@@ -15,7 +15,7 @@ const DUMMY_BOARD_LIST: BoardItemProps[] = [
   {
     title: "테스트2",
     author: "kk1",
-    createdAt: Date.now(),
+    createdAt: new Date("2024-05-20T13:19:11+0000").getTime(),
     viewCount: 10,
     commentCount: 3,
     likeCount: 10,
--- src/modules/Community/components/BoardListItem.tsx
+++ src/modules/Community/components/BoardListItem.tsx
@@ -7,11 +7,30 @@ import {
 import { useState } from "react";
 import type { BoardItemProps } from "../types/Board";
 
+const MONTH_IN_A_YEAR: number = 12;
+
 const BoardListItem = (itemProps: BoardItemProps) => {
   const title = itemProps.title;
   const author = itemProps.author;
-  const createdAt: Date = itemProps.createdAt;
-  const createdAgoMsg = Date.now() - createdAt;
+  const createdAt: number = itemProps.createdAt;
+  const diffMs = Date.now() - createdAt;
+  const diffDate = new Date(diffMs);
+
+  let createdMessage = "";
+  const diffMonth = diffDate.getMonth();
+  const diffYear = Math.floor(diffMonth / MONTH_IN_A_YEAR);
+  const diffDay = diffDate.getDay();
+  const diffHour = diffDate.getHours();
+  const diffMinute = diffDate.getMinutes();
+  if (diffYear > 0) createdMessage = `${diffYear}년 전`;
+  else if (diffMonth > 0) createdMessage = `${diffMonth}개월 전`;
+  else if (diffDay > 0) createdMessage = `${diffDay}일 전`;
+  else if (diffHour > 0) createdMessage = `${diffHour}시간 전`;
+  else if (diffMinute > 0) createdMessage = `${diffMinute}분 전`;
+  else createdMessage = `${diffDate.getSeconds()}초 전`;
+  console.log(diffDate.getMonth());
+  console.log(diffDate.getDay());
+
   const viewCount = itemProps.viewCount;
   const commentCount = itemProps.commentCount;
   const likeCount = itemProps.likeCount;
@@ -23,9 +42,23 @@ const BoardListItem = (itemProps: BoardItemProps) => {
           {title}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          by {author}
+          by {author} {createdMessage}
         </Typography>
-        <Box></Box>
+        <Box display="flex" justifyContent="flex-end">
+          <Box display="flex" mr={1}>
+            <VisibilityOutlined />
+            <Box ml={1}>{viewCount}</Box>
+          </Box>
+
+          <Box display="flex" mr={1}>
+            <ChatBubbleOutlineOutlined />
+            <Box ml={1}>{commentCount}</Box>
+          </Box>
+          <Box display="flex">
+            <FavoriteBorderOutlined />
+            <Box ml={1}>{likeCount}</Box>
+          </Box>
+        </Box>
       </CardContent>
     </Card>
   );
