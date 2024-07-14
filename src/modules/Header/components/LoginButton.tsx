67be9d62f26bf253d687e921bb48ceeb233d06a3
4roring <4roring@naver.com>
2024-07-14 09:57:54 +0900

    react social login buttons 패키지 추가

--- src/modules/Header/components/LoginButton.tsx
+++ src/modules/Header/components/LoginButton.tsx
@@ -1,19 +1,19 @@
-import { Box, IconButton, Modal } from "@mui/material";
-import { Login } from "@mui/icons-material";
-import { useState } from "react";
-import DiscordLoginButton from "../../../components/common/DiscordLoginButton";
+import { Box, IconButton, Modal } from '@mui/material';
+import { Login } from '@mui/icons-material';
+import { useState } from 'react';
+import DiscordLoginButton from '../../../components/common/DiscordLoginButton';
 
 const modalStyle = {
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
     width: 400,
-    bgcolor: "background.paper",
-    border: "0px solid #000",
+    bgcolor: 'background.paper',
+    border: '0px solid #000',
     boxShadow: 24,
     p: 4,
-    textAlign: "center",
+    textAlign: 'center',
 };
 
 const LoginButton = () => {
